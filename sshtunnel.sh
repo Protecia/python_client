9257849dea03b153ef3bead21a5af7a63da33ddd
Julien JEHL <julien.jehl@roboticia.com>
2020-04-28 20:23:38 +0200

    Update sshtunnel.sh

--- sshtunnel.sh
+++ sshtunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 port=`python3 port_ssh_tunnel.py`
-sshpass -p 'protecia2020' autossh -o StrictHostKeyChecking=no -N -R $port:localhost:22 -p 2222 sshtunnel@client.protecia.com &
-sshpass -p 'protecia2020' autossh -o StrictHostKeyChecking=no -N -R $(($port+1)):localhost:2525 -p 2222 sshtunnel@client.protecia.com &
+sshpass -p 'ki4d6z' autossh -o StrictHostKeyChecking=no -N -R $port:localhost:22 -p 2222 cez542de@client.protecia.com &
+sshpass -p 'ki4d6z' autossh -o StrictHostKeyChecking=no -N -R $(($port+1)):localhost:2525 -p 2222 cez542de@client.protecia.com &
