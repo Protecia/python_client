8e4c4955b4c77b89c14cdb58d2aaa71a39613d60
Julien JEHL <julien.jehl@roboticia.com>
2020-06-22 15:51:16 +0200

    Update sshtunnel.sh

--- sshtunnel.sh
+++ sshtunnel.sh
@@ -2,4 +2,4 @@
 port=`python3 port_ssh_tunnel.py`
 sshpass -p 'ki4d6z' autossh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=30 -o ServerAliveCountMax=3 -N -R $port:localhost:22 -p 2222 cez542de@client.protecia.com &
 sshpass -p 'ki4d6z' autossh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=30 -o ServerAliveCountMax=3 -N -R $(($port+1)):localhost:2525 -p 2222 cez542de@client.protecia.com &
-#autossh -M 20000 -C -N -f -n -T -p 2222 -R $port:localhost:22  -R $(($port+1)):localhost:2525  cez542de@client.protecia.com
+#autossh -C -N -f -n -T -p 2222 -R $port:localhost:22  -R $(($port+1)):localhost:2525  cez542de@client.protecia.com
