26523de86e325fc2029f6d336ea679e485caf467
Julien JEHL <julien.jehl@roboticia.com>
2020-08-21 10:39:02 +0200

    ssh update

--- process_camera_thread.py
+++ process_camera_thread.py
@@ -179,7 +179,7 @@ class ProcessCamera(Thread):
                     else:
                         self.request_OK = False
                         self.logger.warning('bad camera download on {} \n'.format(self.cam.name))
-                except (requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+                except (requests.exceptions.Timeout, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout, requests.exceptions.MissingSchema):
                     self.request_OK = False
                     self.logger.warning('network error on {} \n'.format(self.cam.name))
                     pass
