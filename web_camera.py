1a756b469f11a324c550c9c46ff85b894ddb21eb
unknown <julien.jehl@roboticia.com>
2020-11-23 17:08:16 +0100

    Update web_camera.py

--- web_camera.py
+++ web_camera.py
@@ -1,8 +1,8 @@
-from log import Logger
 import settings.settings as settings
 import websockets
 import json
 import asyncio
+
 
 
 class Cameras(object):
@@ -10,13 +10,16 @@ class Cameras(object):
         self.loop = asyncio.get_event_loop()
         self.running = True
         self.camera = None
+        with open(settings.INSTALL_PATH + '/settings/conf.json', 'r') as conf_json:
+            data = json.load(conf_json)
+        self.key = data["key"]
 
     def get_cam(self):
         return self.loop.run_until_complete(self.__async__get_cam())
 
     async def __async__get_cam(self):
         async with websockets.connect(settings.SERVER_WS+'ws') as ws:
-            await ws.send(json.dumps({'key': settings.KEY, 'force': True}))
+            await ws.send(json.dumps({'key': self.key, 'force': True}))
             self.camera = await ws.recv()
 
     def wait_cam(self):
@@ -24,14 +27,14 @@ class Cameras(object):
 
     async def __async__wait_cam(self):
         async with websockets.connect(settings.SERVER_WS + 'ws') as ws:
-            await ws.send(json.dumps({'key': settings.KEY, 'force': False}))
+            await ws.send(json.dumps({'key': self.key, 'force': False}))
             self.camera = await ws.recv()
             await ws.send(json.dumps({'answer': True}))
 
     async def wait_cam_loop(self):
         async with websockets.connect(settings.SERVER_WS+'ws') as ws:
             while True:
-                await ws.send(json.dumps({'key': settings.KEY, 'force': False}))
+                await ws.send(json.dumps({'key': self.key, 'force': False}))
                 self.camera = await ws.recv()
                 await ws.send(json.dumps({'answer': True}))
 
