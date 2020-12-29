56614a535a822e316d55f76718f159e5ec4b4785
unknown <julien.jehl@roboticia.com>
2020-12-29 16:18:41 +0100

    Update log.py

--- log.py
+++ log.py
@@ -8,11 +8,11 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 from settings import settings
-
-#------------------------------------------------------------------------------
+import sys
+# ------------------------------------------------------------------------------
 # a simple config to create a file log - change the level to warning in
 # production
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 class Logger(object):
     def __init__(self, name, level=logging.ERROR):
@@ -22,7 +22,10 @@ class Logger(object):
         file_handler = RotatingFileHandler(os.path.join(settings.INSTALL_PATH+'/camera',name+'.log'), 'a', 10000000, 1)
         file_handler.setLevel(level)
         file_handler.setFormatter(formatter)
+        screen_handler = logging.StreamHandler(stream=sys.stdout)
+        screen_handler.setFormatter(formatter)
         self.logger.addHandler(file_handler)
+        self.logger.addHandler(screen_handler)
 
     def run(self):
         return self.logger
