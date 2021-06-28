2f37b6fbfa5f3a8d110f98cad89f796b5b4dd2f8
Julien <julien.jehl@jouvencia.net>
2021-06-28 15:20:39 +0200

    cron start

--- install_cron.py
+++ install_cron.py
@@ -13,21 +13,21 @@ from settings import settings
 # don't forget to cd for the cron cd /home/protecia/NNvision/python_client &&
 def install_rec_backup_cron():
     cron = CronTab(user=True)
-    if any(cron.find_command('video.py'))==False:
+    if not any(cron.find_command('video.py')):
         cmd = "cd "+settings.INSTALL_PATH+" && "
-        cmd += settings.PYTHON+' '+os.path.join(settings.INSTALL_PATH,'video.py')
+        cmd += settings.PYTHON+' '+os.path.join(settings.INSTALL_PATH, 'video.py')
         cmd += " > "+settings.INSTALL_PATH+"/camera/cron_video.log 2>&1&"
-        job  = cron.new(command=cmd)
+        job = cron.new(command=cmd)
         job.every().hour()
         cron.write()
-        
+
+
 def install_check_tunnel_cron():
     cron = CronTab(user=True)
-    if any(cron.find_command('check_tunnel'))==False:
+    if not any(cron.find_command('check_tunnel')):
         cmd = "sleep 200 && cd "+settings.INSTALL_PATH+" && "
-        cmd += settings.PYTHON+' '+os.path.join(settings.INSTALL_PATH,'check_tunnel.py')
+        cmd += settings.PYTHON+' '+os.path.join(settings.INSTALL_PATH, 'check_tunnel.py')
         cmd += " > "+settings.INSTALL_PATH+"/camera/cron_tunnel.log 2>&1&"
-        job  = cron.new(command=cmd)
+        job = cron.new(command=cmd)
         job.minute.every(10)
         cron.write()
-
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 service cron start
+python3 /NNvision/python_client/install_cron.py
 python3 /NNvision/python_client/main.py
