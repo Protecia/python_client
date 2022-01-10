7050a2b14380772886cd82ee0672985c8b95ad74
Julien <julien.jehl@jouvencia.net>
2022-01-10 18:30:03 +0100

    break task 5

--- process_camera_new.py
+++ process_camera_new.py
@@ -327,6 +327,8 @@ class ProcessCamera(object):
             except (websockets.exceptions.ConnectionClosedError, websockets.exceptions.ConnectionClosedOK,
                     OSError, ConnectionResetError, websockets.exceptions.InvalidMessage)as ex:
                 self.logger.error(f'socket _send_cam disconnected task5 !! / except-->{ex} / name-->{type(ex).__name__}')
+                if not self.running_level1:
+                    break
                 await asyncio.sleep(1)
         self.logger.error('EXIT task5')
 
