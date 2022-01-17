f70d7f3e5392dc4a468c8834f5da1edb6eca8122
Julien <julien.jehl@jouvencia.net>
2022-01-17 21:31:29 +0100

    force reboot

--- conf/check_connection.py
+++ conf/check_connection.py
@@ -1,18 +1,17 @@
 import json
-import settings
 from datetime import datetime
 
 
 def main():
     try:
-        with open(settings.INSTALL_PATH + '/conf/ping.json', 'r') as f:
+        with open('home/nnvision/conf/ping.json', 'r') as f:
             ping = json.load(f)
     except json.decoder.JSONDecodeError:
         print('bad json load, maybe file was writing')
 
     delta_time = datetime.now() - datetime.strptime(ping['last'], '%Y-%m-%d %H:%M:%S')
     if delta_time.total_seconds() > 500:
-        with open(settings.INSTALL_PATH + '/conf/force_reboot.json', 'r') as f:
+        with open('/home/nnvision/conf/force_reboot.json', 'r') as f:
             json.dump({'force_reboot': True, }, f)
 
 
