3fb14b8b2e956ed0b573e5be36118f8335964c0c
Julien <julien.jehl@jouvencia.net>
2022-04-26 11:27:47 +0200

    pass conf

--- conf/patch1.sh
+++ conf/patch1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cat repo.txt > /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
-sudo apt update
-sudo apt dist-upgrade
+sudo apt -y update
+sudo apt  -y dist-upgrade
 echo date > /home/nnvision/patch1.ok
