09a7a38006a0f0873e68badd5321860a4e35502e
Julien <julien.jehl@jouvencia.net>
2022-04-26 21:27:18 +0200

    copy patch

--- patch/patch1.sh
+++ patch/patch1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat repo.txt > /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
+cat /NNvision/python_client/patch/repo.txt > /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
 sudo apt -y update
 sudo apt  -y dist-upgrade
 echo date > /home/nnvision/patch1.ok
