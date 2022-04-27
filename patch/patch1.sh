5fae2adcff0a67e5c25b82f4d44dbda5c8bf7259
Julien <julien.jehl@jouvencia.net>
2022-04-27 08:39:49 +0200

    copy patch

--- patch/patch1.sh
+++ patch/patch1.sh
@@ -2,5 +2,6 @@
 echo "deb https://repo.download.nvidia.com/jetson/common r32.7 main" > /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
 echo "deb https://repo.download.nvidia.com/jetson/t210 r32.7 main"  >> /etc/apt/sources.list.d/nvidia-l4t-apt-source.list
 sudo apt -y update
-sudo apt  -y dist-upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt  -y dist-upgrade
+# maybe yes | apt install foo
 echo date > /home/nnvision/patch1.ok
