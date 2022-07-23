587adeafb52f3b5f77f6c5cea101c03f3781ef40
Julien <julien.jehl@jouvencia.net>
2022-07-23 16:46:29 +0200

    onvif ntp

--- utils/onvif_control.py
+++ utils/onvif_control.py
@@ -8,7 +8,8 @@ def change_ip(on_vif_camera):
 def get_ip(onvif_camera):
     return onvif_camera.devicemgmt.GetNetworkInterfaces()
 
-
+# http://www.onvif.org/wp-content/uploads/2016/12/ONVIF_WG-APG-Application_Programmers_Guide-1.pdf
+# https://github.com/FalkTannhaeuser/python-onvif-zeep
 # time_params = mycam.devicemgmt.create_type('SetSystemDateAndTime')
 # time_params.DateTimeType = 'NTP'
 # time_params.DaylightSavings = True
