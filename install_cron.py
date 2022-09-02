dc4f1f08a9d2f4b7409621c62944212c3e037fe9
Julien <julien.jehl@jouvencia.net>
2022-09-02 08:54:59 +0200

    move check connection to NNvision Startup

--- install_cron.py
+++ install_cron.py
@@ -37,17 +37,6 @@ def install_check_tunnel_cron():
         cron.write()
 
 
-def install_check_connection_cron():
-    cron = CronTab(user=True)
-    if not any(cron.find_command('check_connection')):
-        cmd = "sleep 200 && cd "+settings.INSTALL_PATH+" && "
-        cmd += settings.PYTHON+' '+os.path.join(settings.INSTALL_PATH, 'check_connection.py')
-        cmd += " > "+settings.INSTALL_PATH+"/camera/cron_connection.log 2>&1&"
-        job = cron.new(command=cmd)
-        job.minute.every(10)
-        cron.write()
-
-
 def copy_patch():
     dir1 = Path('/NNvision/python_client/conf/')
     patch_already_copied = [file.name.split('.')[0] for file in dir1.iterdir() if file.name.startswith('patch')]
